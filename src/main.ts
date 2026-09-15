b71ea46c2a4c19d82dc3ed529c919828264cbd21
samiullahnobility <samiullah.nobility@gmail.com>
2026-09-15 14:28:27 -0700

    zone js

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import 'zone.js';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideAnimations } from '@angular/platform-browser/animations';
